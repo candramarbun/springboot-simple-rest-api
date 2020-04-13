47b8b0e9af4bb918915e707e9ed4f876265cfd12
Candra Marbun <you@example.com>
2020-04-13 12:45:48 +0700

    first commit

--- src/main/java/com/marbun/todo/exception/RestErrorHandler.java
+++ src/main/java/com/marbun/todo/exception/RestErrorHandler.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 @ControllerAdvice
 public class RestErrorHandler {
+
     @ExceptionHandler(GeneralException.class)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
