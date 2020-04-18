14920a8ec760ff64e8f765b1833b0c049da57726
Candra Marbun <candramarbun13@gmail.com>
2020-04-18 15:59:41 +0700

    add integration test

--- src/test/java/com/marbun/todo/TodoServiceTest.java
+++ src/test/java/com/marbun/todo/TodoServiceTest.java
@@ -1,5 +1,6 @@
 package com.marbun.todo;
 
+import com.marbun.todo.controllers.TodosController;
 import com.marbun.todo.entities.Todos;
 import com.marbun.todo.repositories.TodoCategorierRepository;
 import com.marbun.todo.repositories.TodoRepository;
@@ -10,16 +11,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 public class TodoServiceTest {
     private Logger logger = LoggerFactory.getLogger(TodoServiceTest.class);
     static Todos todos = new Todos(1, "todo1");
@@ -29,12 +41,18 @@ public class TodoServiceTest {
     TodoCategorierRepository todoCategorierRepository;
     TodoRepository todoRepository;
     TodoCrudServices todoCrudServices;
+    TodosController todosController;
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
 
     @Before
     public void setup() {
         todoCategorierRepository= todoCategorierRepository();
         todoRepository = todoRepository();
-        todoCrudServices = new TodoCrudServicesImpl(todoCategorierRepository, todoRepository);
+        todoCrudServices = todoCrudServices(todoCategorierRepository, todoRepository);
+        todosController = todosController(todoCrudServices);
+        mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
 
     private TodoRepository todoRepository() {
@@ -45,12 +63,21 @@ public class TodoServiceTest {
     }
 
     private TodoCategorierRepository todoCategorierRepository() {
-        TodoCategorierRepository mock = Mockito.mock(TodoCategorierRepository.class);
-        return  mock;
+        return Mockito.mock(TodoCategorierRepository.class);
+    }
+
+    private TodosController todosController(TodoCrudServices todoCrudServices){
+        return new TodosController(todoCrudServices);
+    }
+
+    private TodoCrudServices todoCrudServices(TodoCategorierRepository todoCategorierRepository,TodoRepository todoRepository){
+        TodoCrudServices todoCrudServicesMock = new TodoCrudServicesImpl(todoCategorierRepository,todoRepository);
+        Mockito.when(todoCrudServicesMock.findAllTodos()).thenReturn(TODO_TEST_LIST);
+        return todoCrudServicesMock;
     }
 
     @Test
-    public void test_getAll_Todos(){
+    public void testGetAllTodos(){
         Assert.assertEquals(todoCrudServices.findAllTodos(),TODO_TEST_LIST);
     }
 
@@ -59,4 +86,13 @@ public class TodoServiceTest {
         Assert.assertEquals(todoCrudServices.findById(1),TODO_TEST.get());
     }
 
+    // integration test
+    @Test
+    public void integrationTestGetAllTodos(){
+        try {
+            this.mvc.perform(get("/todos").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
