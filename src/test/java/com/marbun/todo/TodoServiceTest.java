77973f2d41631771dbb7bb7e140890f1f5e82888
Candra Marbun <candramarbun13@gmail.com>
2021-10-18 11:39:07 +0700

    refactor test

--- src/test/java/com/marbun/todo/TodoServiceTest.java
+++ src/test/java/com/marbun/todo/TodoServiceTest.java
@@ -6,15 +6,16 @@ import com.marbun.todo.repositories.TodoCategorierRepository;
 import com.marbun.todo.repositories.TodoRepository;
 import com.marbun.todo.services.TodoCrudServices;
 import com.marbun.todo.services.TodoCrudServicesImpl;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -23,42 +24,50 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 @WebAppConfiguration
 public class TodoServiceTest {
-    private Logger logger = LoggerFactory.getLogger(TodoServiceTest.class);
-    static Todos todos = new Todos(1, "todo1");
-    private static final Optional<Todos> TODO_TEST = Optional.of(todos);
-    private static final List<Todos> TODO_TEST_LIST = Arrays.asList(todos);
-
+    Todos todoTest;
+    List<Todos> todoTestList;
+    @Mock
     TodoCategorierRepository todoCategorierRepository;
+    @Mock
     TodoRepository todoRepository;
+    @Mock
     TodoCrudServices todoCrudServices;
+    @Mock
     TodosController todosController;
     @Autowired
     private WebApplicationContext context;
     private MockMvc mvc;
 
-    @Before
+    @BeforeEach
     public void setup() {
         todoCategorierRepository= todoCategorierRepository();
         todoRepository = todoRepository();
         todoCrudServices = todoCrudServices(todoCategorierRepository, todoRepository);
         todosController = todosController(todoCrudServices);
         mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+
+        //set data
+        Todos todos = new Todos(1, "todo1");
+        todoTest = todos;
+        todoTestList = Collections.singletonList(todos);
     }
 
     private TodoRepository todoRepository() {
         TodoRepository mockTodoRepos = Mockito.mock(TodoRepository.class);
-        Mockito.when(mockTodoRepos.findById(1)).thenReturn(TODO_TEST);
-        Mockito.when(mockTodoRepos.findAll()).thenReturn(TODO_TEST_LIST);
+        Mockito.when(mockTodoRepos.findById(1)).thenReturn(Optional.ofNullable(todoTest));
+        Mockito.when(mockTodoRepos.findAll()).thenReturn(todoTestList);
         return  mockTodoRepos;
     }
 
@@ -72,18 +81,18 @@ public class TodoServiceTest {
 
     private TodoCrudServices todoCrudServices(TodoCategorierRepository todoCategorierRepository,TodoRepository todoRepository){
         TodoCrudServices todoCrudServicesMock = new TodoCrudServicesImpl(todoCategorierRepository,todoRepository);
-        Mockito.when(todoCrudServicesMock.findAllTodos()).thenReturn(TODO_TEST_LIST);
+        Mockito.when(todoCrudServicesMock.findAllTodos()).thenReturn(todoTestList);
         return todoCrudServicesMock;
     }
 
     @Test
     public void testGetAllTodos(){
-        Assert.assertEquals(todoCrudServices.findAllTodos(),TODO_TEST_LIST);
+        assertEquals(todoCrudServices.findAllTodos(),todoTestList);
     }
 
     @Test
     public void findByIdTodo(){
-        Assert.assertEquals(todoCrudServices.findById(1),TODO_TEST.get());
+        assertEquals(todoCrudServices.findById(1),todoTest);
     }
 
     // integration test
